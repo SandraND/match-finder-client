3b166ec75775dba1a184743064badd51af87fcf3
SandraND <lainfaith2@gmail.com>
2018-08-15 15:18:49 +0200

    hide recent groups on login and signup

--- src/app/components/recent-groups/recent-groups.component.ts
+++ src/app/components/recent-groups/recent-groups.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-recent-groups',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecentGroupsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
+
 
 }
