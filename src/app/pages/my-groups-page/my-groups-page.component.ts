052f3327ae39daae874c5a2d63fa48d3b385c955
SandraND <lainfaith2@gmail.com>
2018-08-16 13:06:02 +0200

    game list

--- src/app/pages/my-groups-page/my-groups-page.component.ts
+++ src/app/pages/my-groups-page/my-groups-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { GroupService } from '../../services/group.service';
 
 @Component({
   selector: 'app-my-groups-page',
@@ -7,7 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyGroupsPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private groupService: GroupService
+  ) { }
 
   ngOnInit() {
   }
