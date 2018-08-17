68acc3dfc366ad26e153dd7fa18024027f8cb365
SandraND <lainfaith2@gmail.com>
2018-08-17 09:23:16 +0200

    setup pipe

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -5,8 +5,17 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(items: any[], field: string, value: string): any[] {
+    if (!items) {
+      return [];
+    }
+
+    if (!value) {
+      return items;
+    }
+
+    const myPattern = new RegExp(value, 'i');
+    return items.filter(it => it[field].match(myPattern));
   }
 
 }
