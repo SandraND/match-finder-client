59bdf3933b5504fbb42885a4cd19274a8e7fa105
SandraND <lainfaith2@gmail.com>
2018-08-20 20:41:32 +0200

    display user

--- src/app/components/search-user-detail/search-user-detail.component.ts
+++ src/app/components/search-user-detail/search-user-detail.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
 
 @Component({
   selector: 'app-search-user-detail',
@@ -6,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-user-detail.component.css']
 })
 export class SearchUserDetailComponent implements OnInit {
+  @Input() user: Object;
+  userCurrent: any;
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private authService: AuthService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.userCurrent = this.authService.getUser();
+
+  }
+
+  acceptOne(userId: any) {
+    this.userService.acceptOne(userId, this.userCurrent._id)
+    .then((data) => {
+      this.user = data;
+      this.router.navigate(['/']);
+    });
   }
 
 }
