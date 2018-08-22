15d97697c2f9159a829c6a72f7cf1e934d5b2a72
SandraND <lainfaith2@gmail.com>
2018-08-22 12:59:29 +0200

    add friend list

---
+++ src/app/components/friend-list-all/friend-list-all.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-friend-list-all',
+  templateUrl: './friend-list-all.component.html',
+  styleUrls: ['./friend-list-all.component.css']
+})
+export class FriendListAllComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
